333b97f58f09c3204966989ce666eea8f219364b
anyu <glepooek@outlook.com>
2025-07-17 22:12:43 +0800

    feat(Home): 首页背景

--- VueExamples/vue_apps/digital_book_app/src/views/HomeView.vue
+++ VueExamples/vue_apps/digital_book_app/src/views/HomeView.vue
@@ -1,9 +1,29 @@
+<template>
+  <div class="book-container">
+
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.book-container {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(90deg, #4257C4 0%, #4C90E6 100%);
+}
+
+.book-container::before {
+  content: '';
+  position: absolute;
+  bottom: 82px;
+  left: 0;
+  width: 100%;
+  height: 30%;
+  background: url('../assets/book_background.svg') center / cover no-repeat;
+  /* 确保 SVG 在渐变背景之上 */
+  z-index: 1;
+}
+</style>
