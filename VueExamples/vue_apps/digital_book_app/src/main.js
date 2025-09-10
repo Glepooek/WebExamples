3b5c762349ea498f3825b60cad13f1d65d64f909
anyu <glepooek@outlook.com>
2025-09-10 22:17:37 +0800

    删除无用的组件

--- VueExamples/vue_apps/digital_book_app/src/main.js
+++ VueExamples/vue_apps/digital_book_app/src/main.js
@@ -1,15 +1,15 @@
-import './assets/csss/main.css'
-import 'element-plus/dist/index.css'
-import './assets/svgs/iconfont.js'
-import SvgIcon from './components/icons/SvgIcon.vue'
+import "./assets/csss/main.css"
+import "element-plus/dist/index.css"
+import "./assets/svgs/iconfont.js"
+import SvgIcon from "./components/SvgIcon.vue"
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue"
+import router from "./router"
 
-import ElementPlus from 'element-plus'
+import ElementPlus from "element-plus"
 
 // 打印环境变量、设置标题
 document.title = import.meta.env.VITE_APP_TITLE
@@ -19,6 +19,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
-app.component('svg-icon', SvgIcon)
+app.component("svg-icon", SvgIcon)
 
-app.mount('#app')
+app.mount("#app")
