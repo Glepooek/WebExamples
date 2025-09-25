a61a6207e3a127585eda733127f8d7029713e242
anyu <glepooek@outlook.com>
2025-09-25 18:09:47 +0800

    数字教材点读定位

--- VueExamples/vue_apps/digital_book_app/src/components/ClickRead.vue
+++ VueExamples/vue_apps/digital_book_app/src/components/ClickRead.vue
@@ -1,24 +1,95 @@
 <template>
-  <div class="container" :style="containerStyle" @click="onPlayAudio"></div>
+  <div ref="containerRef" class="container" :style="containerStyle" @click="onPlayAudio" @mouseover="onMouseOver"></div>
 </template>
 
 <script setup>
-  import { computed } from "vue"
+  import { computed, ref, onMounted, watch, nextTick, onBeforeUnmount } from "vue"
   import { getFilePath } from "@/apis/digitalBookApi"
 
   const props = defineProps({
     clickReadModel: { type: Object, required: true },
     moduleName: { type: String, required: true },
     proportion: { type: Number, required: true },
+    activeCids: { type: Array, required: true },
+  })
+
+  const containerRef = ref(null)
+  const imgOffset = ref({ x: 0, y: 0 })
+
+  const calcImgOffset = () => {
+    const el = containerRef.value
+    if (!el || !el.parentElement) return
+    const parent = el.parentElement
+    const img = parent.querySelector("img")
+    if (!img) {
+      imgOffset.value = { x: 0, y: 0 }
+      return
+    }
+
+    // Use bounding rects to be safe (handles transforms)
+    const parentRect = parent.getBoundingClientRect()
+    const imgRect = img.getBoundingClientRect()
+    imgOffset.value = {
+      x: Math.round(imgRect.left - parentRect.left),
+      y: Math.round(imgRect.top - parentRect.top),
+    }
+  }
+
+  onMounted(() => {
+    // initial calc
+    nextTick(calcImgOffset)
+
+    // watch for image load inside parent
+    const el = containerRef.value
+    if (el && el.parentElement) {
+      const img = el.parentElement.querySelector("img")
+      if (img) {
+        img.addEventListener("load", calcImgOffset)
+      }
+    }
+  })
+
+  // recalc when proportion or clickReadModel changes
+  watch(
+    () => props.proportion,
+    () => {
+      nextTick(calcImgOffset)
+    }
+  )
+
+  // Also watch for model changes (in case different image replaced)
+  watch(
+    () => props.clickReadModel,
+    (oldValue, newValue) => {
+      console.log("ClickReadModel changed:", newValue)
+      nextTick(calcImgOffset)
+    }
+  )
+
+  // cleanup
+  onBeforeUnmount(() => {
+    const el = containerRef.value
+    if (el && el.parentElement) {
+      const img = el.parentElement.querySelector("img")
+      if (img) img.removeEventListener("load", calcImgOffset)
+    }
   })
 
   const containerStyle = computed(() => {
+    const baseLeft = (props.clickReadModel.x || 0) * (props.proportion || 1)
+    const baseTop = (props.clickReadModel.y || 0) * (props.proportion || 1)
+    const width = (props.clickReadModel.width || 0) * (props.proportion || 1)
+    const height = (props.clickReadModel.height || 0) * (props.proportion || 1)
+
+    const left = imgOffset.value.x + baseLeft
+    const top = imgOffset.value.y + baseTop
+
     return {
       position: "absolute",
-      left: props.clickReadModel.x * props.proportion + "px",
-      top: props.clickReadModel.y * props.proportion + "px",
-      width: props.clickReadModel.width * props.proportion + "px",
-      height: props.clickReadModel.height * props.proportion + "px",
+      left: left + "px",
+      top: top + "px",
+      width: width + "px",
+      height: height + "px",
     }
   })
 
@@ -35,8 +106,8 @@
 <style scoped>
   .container {
     /* 默认样式可以根据需要添加 */
-    background: yellow;
-    border-color: yellow;
+    background: transparent;
+    border-color: transparent;
     border-radius: 2px;
     border-style: solid;
     border-width: 1px;
